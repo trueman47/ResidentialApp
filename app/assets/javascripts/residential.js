ccb23261011980aeb6d9b0d668218087812e2852
Shantanu K <audentior153@gmail.com>
2017-04-14 18:16:25 +0000

    Bar chart

--- app/assets/javascripts/residential.js
+++ app/assets/javascripts/residential.js
@@ -1,4 +1,4 @@
-$(function(){
+function makePie(){
     // From: http://bl.ocks.org/mbostock/3887235
     // Set the dimensions
     var width  = 960,
@@ -22,7 +22,7 @@ $(function(){
   var svg = d3.select("body").append("svg")
       .attr("width", width)
       .attr("height", height)
-    .append("g")
+      .append("g")
       .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
   // Get the data and draw the slices
@@ -32,7 +32,15 @@ $(function(){
     var g = svg.selectAll(".arc")
         .data(pie(d3.keys(totals)))
       .enter().append("g")
-        .attr("class", "arc");
+        .attr("class", "arc")
+        .on("mouseover",function(d){
+          d3.select(this).select("text").style("font-weight", "bold")
+          d3.select(this).select("text").style("font-size", "1.25em")
+        })
+        .on("mouseout",function(d){
+          d3.select(this).select("text").style("font-weight", "normal")
+          d3.select(this).select("text").style("font-size", "1em")
+        });;
         
      // color the pie slices using the color pallet
     g.append("path")
@@ -40,12 +48,80 @@ $(function(){
         .style("fill", function(d) { return color(d.data); });
 
     // add the jurisdiction name
+   // put the labels outside the pie (in a new arc/circle)
+    var pos = d3.svg.arc().innerRadius(radius + 20).outerRadius(radius + 20);
     g.append("text")
-      .attr("transform", function(d) {
-        return "translate(" + arc.centroid(d) + ")";
-       })
-      .attr("dy", ".35em")
-      .style("text-anchor", "middle")
-      .text(function(d) { return d.data; });    
+    .attr("transform", function(d) {
+      return "translate(" + pos.centroid(d) + ")";
+    })
+    .attr("dy", ".35em")
+    .style("text-anchor", "middle")
+    .text(function(d) { return d.data; });  
   });
-});
+}
+
+
+function makeBar(){
+    var margin = {top: 20, right: 20, bottom: 50, left: 50},
+      width  = 960 - margin.left - margin.right,
+      height = 500 - margin.top - margin.bottom;
+
+      // data -> value
+  var xValue = function(d) { return d.zipcode; },
+      // value -> display
+      xScale = d3.scale.ordinal().rangeRoundBands([0, width],
+         .1),
+      // data -> display
+      xMap   = function(d) { return xScale(xValue(d)); },
+      xAxis  = d3.svg.axis().scale(xScale).orient("bottom");
+
+    // data -> value
+  var yValue = function(d) { return d.median_value; },
+      // value -> display
+      yScale = d3.scale.linear().range([height, 0]),
+      // data -> display
+      yMap   = function(d) { return yScale(yValue(d)); },
+      yAxis  = d3.svg.axis().scale(yScale).orient("left");
+
+  var svg = d3.select("#chart").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  $.getJSON('/residential/bar_data', function(data) {
+    data = data.bar_data;
+    xScale.domain(data.map(xValue));
+    yScale.domain([0, d3.max(data, yValue)]);
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
+      .selectAll("text")
+        .attr("x", 8)
+        .attr("y", -5)
+        .style("text-anchor", "start")
+        .attr("transform", "rotate(90)");
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+      .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Median Value");
+
+    svg.selectAll(".bar")
+        .data(data)
+      .enter().append("rect")
+        .attr("class", "bar")
+       .style("fill", "blue")
+        .attr("x", xMap)
+        .attr("width", xScale.rangeBand)
+        .attr("y", yMap)
+        .attr("height", function(d) { return height - yMap(d); });
+  }); 
+}
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'residential/index'
   get 'residential/data', :defaults => { :format => 'json' }
+  get 'residential/bar_chart'
+  get 'residential/bar_data', :defaults => { :format => 'json' }
   root :to => 'residential#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
