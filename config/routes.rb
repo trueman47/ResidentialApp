ef46eeeb719a5238db5bb7ff02b7eed52b6a123e
Shantanu K <audentior153@gmail.com>
2017-04-16 15:52:48 +0000

    scatterplot

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get 'residential/data', :defaults => { :format => 'json' }
   get 'residential/bar_chart'
   get 'residential/bar_data', :defaults => { :format => 'json' }
+   get 'residential/scatter_chart'
+  get 'residential/scatter_data', :defaults => { :format => 'json' }
   root :to => 'residential#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
